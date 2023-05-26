2aa247f429fd8b7c0521869332de70edd83f6191
Alex-MO <adec.pa.c.man@gmail.com>
2023-05-26 21:07:28 +0000

    update26may23

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -9,6 +9,10 @@ sap.ui.define([
 
         return Controller.extend("DXC.invoices.controller.MainView", {
             onInit: function () {
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("./model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen");
 
             }
         });
